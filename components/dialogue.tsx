f615d20f574c06334cb26d72d5a62dd4086f71e0
klyap <kl.yap@hotmail.com>
2022-12-27 21:26:08 -0800

    fix types and spacing

--- components/dialogue.tsx
+++ components/dialogue.tsx
@@ -1,14 +1,15 @@
 
 import Image from 'next/image'
-import { useEffect, useState, Dispatch } from 'react';
+import { useEffect, useState, Dispatch, SetStateAction } from 'react';
 import styles from '../styles/Home.module.css'
 import { getWelcomeText } from './dialoguetext';
 
 export const Dialogue = () => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState<JSX.Element | string>("");
   const [showOptions, setShowOption] = useState(false);
 
   useEffect(() => {
+    // const textEle = <p>{getWelcomeText()}</p>
     setText(getWelcomeText());
     // console.log(listEngines());
   }, [])
@@ -25,9 +26,18 @@ export const Dialogue = () => {
   )
 }
 
-const Options = ({ setText, setShowOption }: { setText: Dispatch<SetStateAction<string>>, setShowOption: Dispatch<SetStateAction<boolean>> }) => {
-  const creditText = <p>{"This contraption is made with the arcane knowledge embued in"} <a className={styles.link} href={"https://blog.maximeheckel.com/posts/the-magical-world-of-particles-with-react-three-fiber-and-shaders/"}>{" this spellbook about particles and shaders"}</a> and the daemon text-davinci-300 from the realm of OpenAI. </p>;
-  const musicText = <p>{"You are listening to 'little ideology', conjured by the bard "} <a className={styles.link} href={"https://touhoujam.bandcamp.com/album/touhou-jam-jams"}>{'iceerules'}</a>. {'Other sound effects are from a long forgotten source (I am so sorry >_<). Historians surmise that it may have been from a free itch.io Gameboy Sound Effect pack. '}</p>;
+const Options = ({ setText, setShowOption }: { setText: Dispatch<SetStateAction<JSX.Element | string>>, setShowOption: Dispatch<SetStateAction<boolean>> }) => {
+  const creditText = <p>
+    {"This contraption is made with the arcane knowledge embued in"}
+    <a className={styles.link} href={"https://blog.maximeheckel.com/posts/the-magical-world-of-particles-with-react-three-fiber-and-shaders/"}>
+      {" this spellbook about particles and shaders"}
+    </a>
+    {" and the daemon text-davinci-300 from the realm of OpenAI. "}</p>;
+  const musicText = <p>
+    {"You are listening to 'little ideology', conjured by the bard "}
+    <a className={styles.link} href={"https://touhoujam.bandcamp.com/album/touhou-jam-jams"}>{'iceerules'}</a>.
+    {'Other sound effects are from a long forgotten source (I am so sorry >_<). Historians surmise that it may have been from a free itch.io Gameboy Sound Effect pack. '}
+  </p>;
 
   return (<div className={styles.dialogueOptionContainer}>
     <p className={styles.dialogueOption} onClick={() => { setText(creditText); setShowOption(false) }}>{"How... how is this possible?"}</p>
